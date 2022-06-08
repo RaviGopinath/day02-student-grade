7833bb186839846235c26c8c52f3a62ab3746877
RaviGopinath <gopinathravi95@gmail.com>
2022-06-08 17:41:12 +0530

    upload

---
+++ studentGrade.js
@@ -0,0 +1,22 @@
+let gopi;
+
+gopi = 80;
+
+if(gopi >= 90){
+    alert("Gopi is S grade");
+}
+else if(gopi >= 80){
+    alert("Gopi is A grade");
+}
+else if(gopi >= 70){
+    alert("Gopi is B grade");
+}
+else if(gopi >= 60){
+    alert("Gopi is C grade");
+}
+else if(gopi >= 50){
+    alert("Gopi is D grade");
+}
+else{
+    alert("Gopi is Fail");
+}
